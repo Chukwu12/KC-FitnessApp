ccbe84e91f3a11c8efefe67230f7edd9a88a3dd1
Okechukwu Egeruoh <oegeruoh@gmail.com>
2025-12-25 15:01:03 -0500

    Fix Expo/Sanity setup and dependency updates

--- src/lib/sanity.ts
+++ src/lib/sanity.ts
@@ -1,11 +1,8 @@
 import { createClient } from "@sanity/client";
-import Constants from "expo-constants";
-
-const { SANITY_PROJECT_ID, SANITY_DATASET } = Constants.expoConfig?.extra ?? {};
 
 export const client = createClient({
-  projectId: SANITY_PROJECT_ID,
-  dataset: SANITY_DATASET,
+  projectId: process.env.EXPO_PUBLIC_SANITY_PROJECT_ID!,
+  dataset: process.env.EXPO_PUBLIC_SANITY_DATASET!,
   apiVersion: "2024-01-01",
-  useCdn: false,
+  useCdn: true,
 });
